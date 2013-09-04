c1fa2723716e2b0f1d95888df4e937a3b6fdabb3
yikebocai <yikebocai@gmail.com>
2013-09-04 13:31:49 +0800

    rm jars that gid and aid are same

--- check_jar_conflict.sh
+++ check_jar_conflict.sh
@@ -12,16 +12,15 @@ if [[ -n $build ]]; then
    exit
 fi
 
-echo "[info] get all dependencies jar"
+echo "[info] get all dependencies jar and check conflicted class files"
 dep_jar_list=`cat dt |grep INFO|grep jar|grep compile|awk -F '(' '{print $1}'|awk -F ' ' '{print $NF}'|awk -F ':' '{gsub(/\./,"/",$1);print $1"/"$2"/"$4"/"$2"-"$4"."$3}'|sort|uniq`
-
-echo "[info] check all class files"
+ 
 total=0
 declare -A jar_map
 for dep_jar in $dep_jar_list
 do
   dep_jar="$M2_REPO$dep_jar"
-  #echo "[debug] $dep_jar"
+  #echo "[debug] dep_jar: $dep_jar"
   if [ -f $dep_jar ]; then
 
      #list all classes in jar 
@@ -29,17 +28,30 @@ do
      for class in $class_list
      do      
 
-      jar_path=${jar_map[$class]}
+      jar_path=${jar_map[$class]} 
       if [[ -z $jar_path  ]]; then
         jar_map[$class]="$dep_jar"
-        #echo "[debug]    $class"    
+
+      # ignore same path jars
       elif [[ $jar_path != $dep_jar ]]; then
-        total=$(( $total + 1 ))
-        echo "[warn] conflict class is $class"
-        echo "[warn] 1 : $dep_jar"
-        echo "[warn] 2 : $jar_path"
-        echo " "  
-        break
+        # cut the gid and aid part
+        suffix1=`echo $jar_path|awk -F '/' '{print $(NF-1)$NF}'` 
+        ((len1=${#jar_path} - ${#suffix1} - 2))
+        gaid1=${jar_path:0:$len1} 
+
+        suffix2=`echo $dep_jar|awk -F '/' '{print $(NF-1)$NF}'` 
+        ((len2=${#dep_jar} - ${#suffix2} - 2)) 
+        gaid2=${dep_jar:0:$len2} 
+
+        # ignore jars that gid and aid are same
+        if [[ $gaid1 != $gaid2 ]]; then
+          total=$(( $total + 1 ))
+          echo "[warn] conflict class is $class"
+          echo "[warn] 1: $dep_jar"
+          echo "[warn] 2: $jar_path"
+          echo " "  
+          break 
+        fi
       fi
      done 
   fi
@@ -47,4 +59,5 @@ done
 
 echo "[warn] total conflicted jar  is $total"
 
+#remove the temp dependency tree file
 rm dt
